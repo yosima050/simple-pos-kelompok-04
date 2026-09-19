d63105e66805f8b0c129ab4cbcd59293816bce62
NewUserWIDI <wwidi4087@gmail.com>
2026-09-19 23:07:58 +0700

    perbaiki tampilan produk agar tidak duplikat

--- resources/views/pos/create.blade.php
+++ resources/views/pos/create.blade.php
@@ -13,18 +13,14 @@ return this.cart.reduce((sum, item) => sum + item.price, 0);
 }
 }">
 <div class="grid grid-cols-3 gap-4">
-@foreach ($products as $product)
-<div class="grid grid-cols-3 gap-4">
     @foreach ($products as $product)
         <div class="border rounded-md p-3 cursor-pointer"
          :class="selectedProductId === {{ $product->id }} ? 'ring-2 ring-blue-500' : ''"
-             @click="selectedProductId = {{ $product->id }}; addToCart({{ $product->id }}, '{{ $product->name }}', {{ $product->price }})"
+             @click="selectedProductId = {{ $product->id }}; addToCart({{ $product->id }}, '{{ $product->name }}', {{ $product->price }})">
             <p class="font-medium">{{ $product->name }}</p>
             <p class="text-sm text-slate-500">Rp {{ number_format($product->price) }}</p>
         </div>
     @endforeach
-</div>
-@endforeach
 </div>
 <div class="mt-4 border-t pt-3">
 <template x-for="item in cart" :key="item.id">
